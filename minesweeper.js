e49a2af2d794cdda8ceba0cf213a9ea65610da12
Murilo Polese <murilopolese@gmail.com>
2013-11-22 12:27:09 -0200

    Bomb counter for a single bomb

--- minesweeper.js
+++ minesweeper.js
@@ -10,20 +10,69 @@ var game = {
         }
         return matrix;
     },
-    verifyLimit: function(matrix, x, y) {
-        if(x < matrix.length-1 && x >= 0 &&
-           y < matrix.length-1 && y >= 0) {
+    verifySpaceAround: function(matrix, x, y) {
+        if( this.verifySpaceAbove(matrix, y)  
+            && this.verifySpaceBelow(matrix, y) 
+            && this.verifySpaceRight(matrix, x) 
+            && this.verifySpaceLeft(matrix, x)
+            ) {
             return true;
         }
         return false;
     },
     putBomb: function(matrix, x, y) {
-        if(this.verifyLimit(matrix, x, y)) {
+        if(this.verifySpaceAround(matrix, x, y)) {
             matrix[x][y] = this.bomb;
         }
         return matrix;
     },
-
+    verifySpaceAbove: function(matrix, y) {
+        if(y > 0) {
+            return true;
+        }
+        return false;
+    },
+    verifySpaceBelow: function(matrix, y) {
+        if(matrix[0] != undefined &&
+            y < matrix[0].length-1) {
+            return true;
+        }
+        return false;
+    },
+    verifySpaceRight: function(matrix, x) {
+        if(x < matrix.length-1) {
+            return true;
+        }
+        return false;
+    },
+    verifySpaceLeft: function(matrix, x, y) {
+        if(x > 0) {
+            return true;
+        }
+        return false;
+    },
+    bombCounter: function(matrix, x, y) {
+        if(this.verifySpaceAbove(matrix, y)) {
+            matrix[x-1][y-1]++;
+            matrix[x][y-1]++;
+            matrix[x+1][y-1]++;
+        }
+        if(this.verifySpaceLeft(matrix, x)) {
+            // Add counter left
+            matrix[x-1][y]++;
+        }
+        if(this.verifySpaceRight(matrix, x)) {
+            // Add counter right
+            matrix[x+1][y]++;
+        }
+        if(this.verifySpaceBelow(matrix, y)) {
+            // Add counter below
+            matrix[x-1][y+1]++;
+            matrix[x][y+1]++;
+            matrix[x+1][y+1]++;
+        }
+        return matrix;
+    }
 
 }
 module.exports = game;
--- tests/test.js
+++ tests/test.js
@@ -9,15 +9,60 @@ describe('Matrix operations', function() {
             done(new Error('Matrix with wrong dimensions.'));
         }
     });
-    it('verify limits', function(done) {
-        var matrix = game.createMatrix(4, 3);
-        if(game.verifyLimit(matrix, 1, 1) &&
-            !game.verifyLimit(matrix, 1, 4)) {
+    it('check if there is space above', function(done) {
+        var matrix = game.createMatrix(3, 3);
+        if(game.verifySpaceAbove(matrix, 1) &&
+            !game.verifySpaceAbove(matrix, 0)) {
             done();
         } else {
-            done(new Error('Matrix boundaries verification not working.'));
+            done('Space above verification is wrong.');
+        }
+    });
+    it('check if there is space below', function(done) {
+        var matrix = game.createMatrix(3, 3);
+        if(game.verifySpaceBelow(matrix, 1) &&
+            !game.verifySpaceBelow(matrix, 2)) {
+            done();
+        } else {
+            done('Space below verification is wrong.');
+        }
+    });
+    it('check if there is space right', function(done) {
+        var matrix = game.createMatrix(3, 3);
+        if(game.verifySpaceRight(matrix, 1) &&
+            !game.verifySpaceRight(matrix, 2)) {
+            done();
+        } else {
+            done('Space right verification is wrong.');
+        }
+    });
+    it('check if there is space left', function(done) {
+        var matrix = game.createMatrix(3, 3);
+        if(game.verifySpaceLeft(matrix, 1) &&
+            !game.verifySpaceLeft(matrix, 0)) {
+            done();
+        } else {
+            done('Space left verification is wrong.');
+        }
+    });
+    it('check space around', function(done) {
+        var matrix = game.createMatrix(3, 3);
+        if(  game.verifySpaceAround(matrix, 1, 1) 
+            && !game.verifySpaceAround(matrix, 0, 0)
+            && !game.verifySpaceAround(matrix, 0, 1) 
+            && !game.verifySpaceAround(matrix, 0, 2) 
+            && !game.verifySpaceAround(matrix, 1, 0) 
+            && !game.verifySpaceAround(matrix, 1, 2) 
+            && !game.verifySpaceAround(matrix, 2, 0) 
+            && !game.verifySpaceAround(matrix, 2, 1) 
+            && !game.verifySpaceAround(matrix, 2, 2)
+            ) {
+            done();
+        } else {
+            done(new Error('Space around verification is wrong.'));
         }
     })
+
     it('put bomb', function(done) {
         var matrix = game.createMatrix(4, 3);
         matrix = game.putBomb(matrix, 1, 1);
@@ -25,6 +70,24 @@ describe('Matrix operations', function() {
             done();
         } else {
             done(new Error('Bomb hasn\'t been planted. Counter terrorists win.'));
+        }
+    });
+    
+    it('add bomb counter around for one bomb', function(done) {
+        var matrix = game.createMatrix(4, 4);
+        matrix = game.putBomb(matrix, 1, 1);
+        matrix = game.bombCounter(matrix, 1, 1);
+        if(
+            // Check above
+            matrix[0][0] == 1 && matrix[1][0] == 1 && matrix[2][0] == 1
+            // check right and left
+            && matrix[0][1] == 1 && matrix[2][1]
+            // check below
+            && matrix[0][2] == 1 && matrix[1][2] == 1 && matrix[2][2] == 1
+        ) {
+            done();
+        } else {
+            done(new Error('Counter is wrong.'));
         }
     })
 })
