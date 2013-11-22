ae4514aa463a89833c4a051abfed279ccbda85bb
Murilo Polese <murilopolese@gmail.com>
2013-11-22 11:24:44 -0200

    Bomb has been planted. Terrorist wins.

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,5 @@
 var game = {
+    bomb: 'bomb',
     createMatrix: function(w, h) {
         var matrix = new Array(w);
         for(var x = 0; x < w; x++) {
@@ -8,7 +9,21 @@ var game = {
             }
         }
         return matrix;
-    }
+    },
+    verifyLimit: function(matrix, x, y) {
+        if(x < matrix.length-1 && x >= 0 &&
+           y < matrix.length-1 && y >= 0) {
+            return true;
+        }
+        return false;
+    },
+    putBomb: function(matrix, x, y) {
+        if(this.verifyLimit(matrix, x, y)) {
+            matrix[x][y] = this.bomb;
+        }
+        return matrix;
+    },
+
 
 }
 module.exports = game;
--- tests/test.js
+++ tests/test.js
@@ -9,4 +9,22 @@ describe('Matrix operations', function() {
             done(new Error('Matrix with wrong dimensions.'));
         }
     });
+    it('verify limits', function(done) {
+        var matrix = game.createMatrix(4, 3);
+        if(game.verifyLimit(matrix, 1, 1) &&
+            !game.verifyLimit(matrix, 1, 4)) {
+            done();
+        } else {
+            done(new Error('Matrix boundaries verification not working.'));
+        }
+    })
+    it('put bomb', function(done) {
+        var matrix = game.createMatrix(4, 3);
+        matrix = game.putBomb(matrix, 1, 1);
+        if(matrix[1][1] == game.bomb) {
+            done();
+        } else {
+            done(new Error('Bomb hasn\'t been planted. Counter terrorists win.'));
+        }
+    })
 })
